ec340debaa6671c0108983c687e687ce3f953fcb
kmfb21 <kmfb21@gmail.com>
2016-04-26 18:15:36 -0400

    runtime fixed from 180ms to 30ms. still need improve algorithm

--- DistinctSubsequences.java
+++ DistinctSubsequences.java
@@ -1,23 +1,23 @@
 public class Solution {
     static int[][] dp,dps;
-    static String ss,tt;
+    static char[] ss,tt;
     static int solve(int si,int ti) {
         if(dps[si][ti]==1) return dp[si][ti];
         dps[si][ti] = 1;
-        if(ti==tt.length()) {
+        if(ti==tt.length) {
             dp[si][ti] = 1;
             return 1;
         }
-        String tc = tt.substring(ti,ti+1);
-        for(int i=si;i<ss.length();i++) {
-            if(tc.equals(ss.substring(i,i+1)))
+        char tc = tt[ti];
+        for(int i=si;i<ss.length;i++) {
+            if(tc==ss[i])
                 dp[si][ti] += solve(i+1,ti+1);
         }
         return dp[si][ti];
     }
     public int numDistinct(String s, String t) {
-        ss = s;
-        tt = t;
+        ss = s.toCharArray();
+        tt = t.toCharArray();
         dp = new int[s.length()+1][t.length()+1];
         dps = new int[s.length()+1][t.length()+1];
         return solve(0,0);
